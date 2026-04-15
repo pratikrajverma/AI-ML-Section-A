5524d9d91484bc4546c5ac333b822e4b1c606b53
Pratik Raj Verma <erpratikrajverma@gmail.com>
2026-04-15 15:49:13 +0530

    react router

--- Day 20 Unit 4/myapp/src/App.jsx
+++ Day 20 Unit 4/myapp/src/App.jsx
@@ -3,11 +3,21 @@ import Child from './components/Child'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Dashboard from './pages/Dashboard'
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link, NavLink, Route, Routes, useNavigate } from 'react-router-dom'
 
 const App = () => {
 
+   const navigate = useNavigate()
+
+
+  function aboutHandler(){
+      navigate('/about')
+  }
  
+
+  function contactHandler(){
+    navigate('/contact')
+  }
 
   return (
     <div>
@@ -17,13 +27,24 @@ const App = () => {
       <p>Contact</p>
       <p>Dashboard</p>   */}
 
- 
+{/*  
         <Link to='/about'>About</Link><br/><br/>
         <Link to='/contact'>Contact</Link><br/><br/>
         <Link to='/dashboard'>Dashboard</Link><br/><br/>
+        <Link to='/'>Home</Link><br/><br/> */}
 
-        <Link to='/'>Home</Link>
-        
+
+        <NavLink to='/about'> About </NavLink><br/><br/>
+        <NavLink to='/contact'> Contact </NavLink><br/><br/>
+        <NavLink to='/dashboard'> Dashboard </NavLink><br/><br/>
+
+
+
+        <button onClick={aboutHandler}>About</button>
+        <button onClick={contactHandler}>contact</button>
+        <button>Dashboard</button>
+
+   
 
         <Routes>
 
