4d8aa84070e3eff4adadb5c126cfa0e122a23b65
Pratik Raj Verma <erpratikrajverma@gmail.com>
2026-04-23 14:40:14 +0530

    #

--- Day 20, 21,22  Unit 4/myapp/src/App.jsx
+++ Day 20, 21,22  Unit 4/myapp/src/App.jsx
@@ -5,7 +5,7 @@ import Contact from './components/React Router/pages/Contact'
 import Dashboard from './components/React Router/pages/Dashboard'
 import { Form, Link, NavLink, Route, Routes, useNavigate, useSearchParams } from 'react-router-dom'
 import FormBox from './components/React Form/FormBox'
- 
+
 import Card1 from './components/Context api Cards/cards/Card1'
 import Card2 from './components/Context api Cards/cards/Card2'
 import Card3 from './components/Context api Cards/cards/Card3'
@@ -17,7 +17,7 @@ import Home from './components/React Router/pages/Home'
 
 const App = () => {
 
-  
+
 
 
 
@@ -26,56 +26,45 @@ const App = () => {
 
   return (
     <div  >
-      {/* <Child/> */}
 
-    
-
-
-
-
-
-     {/* api calling examples.............. */}
+      {/* api calling examples.............. */}
       {/* <ProductList/> */}
 
 
 
-    {/* context api examples..... */}
+      {/* context api examples..... */}
 
       {/* <ParentCard/> */}
-      
 
-    {/* React Form................. */}
+
+      {/* React Form................. */}
       {/* <FormBox/> */}
 
 
 
       {/* React Router.............. */}
 
-        {/* <Home/> */}
+      {/* <Home/> */}
 
 
 
       {/* Use effect hook using....... */}
 
-        {/* <Child/> */}
+      {/* <Child/> */}
 
 
 
 
 
 
-    {/* examples for types of components rendering...... */}
+      {/* examples for types of components rendering...... */}
 
       {/* <Card3 dark={dark} />
-
 
 
             <Card3>
               {dark}
             </Card3> */}
-
-
-
 
     </div>
   )
